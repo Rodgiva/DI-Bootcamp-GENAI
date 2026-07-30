ee9fb886c95c57421cf6ce7d571b0f28464d3a38
Avigdor <avigdorpartouche@gmail.com>
2026-07-30 11:36:12 +0300

    correction exercice list

--- Week0/Python_list/Exercice.py
+++ Week0/Python_list/Exercice.py
@@ -1,69 +1,53 @@
-list = [1,2,3,4]
-for i in list:
-    print(i)
+# --- 1 ---
+input_list = [1,2,3,4]
+[print(n) for n in input_list]
+print("----------------")
 
-list = [1,2,3,4]
-for i in list:
-    print(i*20)
+# --- 2 ---
+input_list = [1,2,3,4]
+[print(n*20) for n in input_list]
+print("----------------")
 
-list = ["Elie", "Tim", "Matt"]
-for i in list:
-    print(i.title())
+# --- 3 ---
+input_list = ["Elie", "Tim", "Matt"]
+print([i[0] for i in input_list])
+print("----------------")
 
-list = [1,2,3,4,5,6]
-res = []
-for i in list:
-    if i % 2 == 0:
-        res.append(i)
-print(res)
+# --- 4 ---
+input_list = [1,2,3,4,5,6]
+print([n for n in input_list if n % 2 == 0])
+print("----------------")
 
-list1 = [1,2,3,4]
-list2 = [3,4,5,6]
+# --- 5 ---
+input_list1 = [1,2,3,4]
+input_list2 = [3,4,5,6]
+print([n for n in input_list1 if n in input_list2])
+print("----------------")
 
-print([
-    x
-    for x in list1
-    if x in list2
-])
+# --- 6 ---
+input_list = ["Elie", "Tim", "Matt"]
+print([i.lower()[::-1] for i in input_list])
+print("----------------")
 
-list = ["Elie", "Tim", "Matt"]
-res = []
-for i in list:
-    res.append(i.lower()[::-1])
-print(res)
-
+# --- 7 ---
 str1 , str2 = "first", "third"
-str = str1 + str2
-print([
-    x
-    for x in str
-    if x in ["i", "r", "t"]
-])
+input_str = str1 + str2
+print("".join([x for x in input_str if x in ["i", "r", "t"]]))
+print("----------------")
 
-print([
-    x
-    for x in range(1, 100)
-    if x % 12 == 0
-])
+# --- 8 ---
+print([x for x in range(1, 100) if x % 12 == 0])
+print("----------------")
 
-print([
-    x
-    for x in "amazing"
-    if x not in ["a", "i", "e", "o", "u"]
-])
+# --- 9 ---
+print([x for x in "amazing"if x not in ["a", "i", "e", "o", "u"]])
+print("----------------")
 
-list = []
-for i in range(0, 3):
-    list_ = []
-    for j in range(0, 3):
-        list_.append(j)
-    list.append(list_)
-print(list)
+# --- 10 ---
+input_list = []
+print([[i for i in range(0,3)] for j in range(0, 3)])
+print("----------------")
 
-list = []
-for i in range(10):
-    list_ = []
-    for j in range(10):
-        list_.append(j)
-    list.append(list_)
-print(list)
+# --- 11 ---
+input_list = []
+print([[i for i in range(0,10)] for j in range(0, 10)])
