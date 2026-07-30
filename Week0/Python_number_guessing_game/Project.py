3df561d470c265beee14003fb4892b5f8f34f1cd
Avigdor <avigdorpartouche@gmail.com>
2026-07-30 10:46:02 +0300

    correction python game

--- Week0/Python_number_guessing_game/Project.py
+++ Week0/Python_number_guessing_game/Project.py
@@ -3,19 +3,23 @@ import random
 def number_guessing_game():
     nb_user = -1
     max_attempts = 7
-    random_number = random.randint(0, 100)
+    random_number = random.randint(1, 100)
     for i in range(max_attempts):
-        # while 0 <= nb_user <= 100:
-        nb_user = int(input("Give me a number between 0 and 100:"))
-            # if nb_user > 100 or nb_user < 0:
-            #     print("Not in range, try again!")
+        try:
+            nb_user = int(input("Give me a number between 1 and 100:"))
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            return
         if nb_user < random_number:
             print("Too low!")
         elif nb_user > random_number:
             print("Too high!")
         elif nb_user == random_number:
             print("Congratulations! You guessed the right number!")
+            print(f"Number times of attempt: {i+1}")
             return
+        
+    print(f"The right number was {random_number}")
     print("Game Over")
     return
     
