a9829459757d07dcfbf6ba2f45b01428bfa4f7a4
Avigdor <avigdorpartouche@gmail.com>
2026-08-05 15:05:05 +0300

    week1 day5 correction DailyChallengeAdvanced

--- Week1/Day5/DailyChallenge/DailyChallengeAdvanced.py
+++ Week1/Day5/DailyChallenge/DailyChallengeAdvanced.py
@@ -3,11 +3,11 @@ import random
 list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
 target_number   = 3728
 paired = []
+print(len(list_of_numbers))
+list_of_numbers = list(set(list_of_numbers))
+print(len(list_of_numbers))
 
 for i in range(len(list_of_numbers)-1):
-    # an indicator to confirm that de program is running
-    if i % 100 == 0:
-        print(f"i:{i}")
     for j in range(i+1, len(list_of_numbers)):
         if list_of_numbers[i] + list_of_numbers[j] == target_number:
             paired.append((list_of_numbers[i], list_of_numbers[j]))
