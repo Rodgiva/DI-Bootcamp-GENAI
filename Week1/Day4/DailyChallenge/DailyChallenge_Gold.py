f0f0255d773564910a1bfc7bb6b4ed9188a29d3e
Avigdor <avigdorpartouche@gmail.com>
2026-08-05 10:08:39 +0300

    week1 day4 correction DailyChallengeGold

--- Week1/Day4/DailyChallenge/DailyChallenge_Gold.py
+++ Week1/Day4/DailyChallenge/DailyChallenge_Gold.py
@@ -7,23 +7,22 @@ sM#
 $a 
 #t%'''
 
-# --- Step 1: Transforming the String into a 2D List  ---
 matrix_list = MATRIX_STR.split("\n")
 matrix_list = [line for line in matrix_list if line]
 print(matrix_list)
 print("--------------------------")
 
-# --- Step 2: Processing Columns  ---
-# --- Step 3: Filtering Alpha Characters  ---
-# --- Step 4: Replacing Symbols with Spaces  ---
-# --- Step 5: Constructing the Secret Message ---
 msg = ""
+flag = False
 for i in range(len(matrix_list[0])):
     for j in range(len(matrix_list)):
-        if 65 <= ord(matrix_list[j][i]) <= 122:
+        if matrix_list[j][i].isalpha():
+            if flag:
+                msg += " "
+                flag = False
             msg += matrix_list[j][i]
         else:
-            msg += " "
+                flag = True
 print(msg)
 print("--------------------------")
 
