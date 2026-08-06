fa0e4721106ecf20057e18e6babb9afa18a6074b
Avigdor <avigdorpartouche@gmail.com>
2026-08-06 13:05:27 +0300

    week1 day4 improve the matrix challenge

--- Week1/Day4/DailyChallenge/DailyChallenge_Gold.py
+++ Week1/Day4/DailyChallenge/DailyChallenge_Gold.py
@@ -1,3 +1,28 @@
+# MATRIX_STR = '''
+# 7ir
+# Tsi
+# h%x
+# i ?
+# sM# 
+# $a 
+# #t%'''
+
+# matrix_list = MATRIX_STR.split("\n")
+# matrix_list = [line for line in matrix_list if line]
+
+# msg = ""
+# flag = False
+# for i in range(len(matrix_list[0])):
+#     for j in range(len(matrix_list)):
+#         if matrix_list[j][i].isalpha():
+#             if flag:
+#                 msg += " "
+#                 flag = False
+#             msg += matrix_list[j][i]
+#         else:
+#             flag = True
+# msg = msg.strip()
+
 MATRIX_STR = '''
 7ir
 Tsi
@@ -7,22 +32,18 @@ sM#
 $a 
 #t%'''
 
-matrix_list = MATRIX_STR.split("\n")
-matrix_list = [line for line in matrix_list if line]
-print(matrix_list)
-print("--------------------------")
+def matrix_decoder(encoded_str: str)->str:
+    matrix_list = encoded_str.split("\n")
+    matrix_list = [line for line in matrix_list if line]
+    res = ""
+    for i in range(len(matrix_list[0])):
+        for j in range(len(matrix_list)):
+            if matrix_list[j][i].isalpha():
+                res += matrix_list[j][i]
+            else:
+                res += " "
+    return " ".join(res.split())
 
-msg = ""
-flag = False
-for i in range(len(matrix_list[0])):
-    for j in range(len(matrix_list)):
-        if matrix_list[j][i].isalpha():
-            if flag:
-                msg += " "
-                flag = False
-            msg += matrix_list[j][i]
-        else:
-                flag = True
-print(msg)
-print("--------------------------")
+print(matrix_decoder(MATRIX_STR))
+# print("--------------------------")
 
