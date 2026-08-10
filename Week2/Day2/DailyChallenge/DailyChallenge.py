5396254108bc81db09918da73009ad4bbd25bb7f
Avigdor <avigdorpartouche@gmail.com>
2026-08-10 14:37:14 +0300

    week2 day2 DailyChallenge

---
+++ Week2/Day2/DailyChallenge/DailyChallenge.py
@@ -0,0 +1,64 @@
+import math as m
+
+class Pagination():
+    def __init__(self, page_size:int = 10, items = None):
+        self.page_size = page_size
+        self.items = [] if items == None else list(items)
+        
+        self._current_idx = 0
+        self._nb_page = m.ceil(len(items)/self.page_size)
+
+    def  get_visible_items(self)->list:
+        return self.items[self.page_size*self._current_idx : self.page_size*(self._current_idx+1)]
+
+    def go_to_page(self, page_num:int):
+        if (page_num * self.page_size) + self.page_size-1 > len(self.items):
+            raise ValueError("The page number is out of range")
+        self._current_idx = page_num - 1
+        return self
+
+    def first_page(self):
+        self._current_idx = 0
+        return self
+
+    def last_page(self):
+        self._current_idx = len(self.items)//self.page_size
+        return self
+
+    def next_page(self):
+        if self._current_idx != len(self.items)//self.page_size:
+            self._current_idx += 1
+        return self
+
+    def previous_page(self):
+        if self._current_idx > 0:
+            self._current_idx -= 1
+        return self
+
+    def __str__(a_list:list, length:int):
+        return "".join(a_list[:length-1])
+
+
+alphabetList = list("abcdefghijklmnopqrstuvwxyz")
+p = Pagination(4, alphabetList)
+
+print(p.get_visible_items())
+# ['a', 'b', 'c', 'd']
+
+p.next_page()
+print(p.get_visible_items())
+# ['e', 'f', 'g', 'h']
+
+p.last_page()
+print(p.get_visible_items())
+# ['y', 'z']
+
+# p.go_to_page(10)
+# print(p.current_idx + 1)
+# Output: ValueError
+
+p.go_to_page(0)
+# Raises ValueError
+
+p.next_page().next_page().next_page().get_visible_items()
+print(p.get_visible_items())
